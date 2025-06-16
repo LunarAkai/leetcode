8e14ef07027fb27b551787c4bc667af965daa070
LunarAkai <akai@lunarakai.de>
2025-06-16 13:29:11 +0200

    2016. Maximum Difference Between Increasing Elements

---
+++ maximum_difference_between_increasing_elements/MaxDifferenceLeetcode/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public class Solution
+{
+
+    public static void Main()
+    {
+        int[] nums1 = [7, 1, 5, 4];     // Output: 4    (1 - 5)
+        int[] nums2 = [9, 4, 3, 2];     // Output: -1   (no i < j)
+        int[] nums3 = [1, 5, 2, 10];    // Output: 9    (1 - 10)
+        Console.WriteLine("Hello World");
+
+        var solution = new Solution();
+        Console.WriteLine(solution.MaximumDifference(nums1));
+        Console.WriteLine(solution.MaximumDifference(nums2));
+        Console.WriteLine(solution.MaximumDifference(nums3));
+    }
+    public int MaximumDifference(int[] nums)
+    {
+        int difference = 0;
+        int maxDifference = -1;
+
+        foreach (int i in nums)
+        {
+            foreach (int j in nums)
+            {
+                if (j < i)
+                {
+                    continue;
+                }
+
+                if (Array.IndexOf(nums, i) > Array.IndexOf(nums, j))
+                {
+                    continue;
+                }
+                difference = j - i;
+
+                if (difference > maxDifference)
+                {
+                    maxDifference = difference;
+                }
+            }
+        }
+
+        if (maxDifference == 0)
+        {
+            return -1;
+        }
+        return maxDifference;
+    }
+}
